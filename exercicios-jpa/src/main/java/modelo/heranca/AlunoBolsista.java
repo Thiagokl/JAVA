83c28b6230ed6f631eb10d17b450f8d412cd3b90
Thiago Costa Santana <thikimlara@gmail.com>
2024-08-09 16:33:50 -0300

    commit Exercicios jpa

---
+++ exercicios-jpa/src/main/java/modelo/heranca/AlunoBolsista.java
@@ -0,0 +1,29 @@
+package modelo.heranca;
+
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+
+@Entity
+@DiscriminatorValue("AB")
+public class AlunoBolsista extends Aluno {
+
+	private double valorbolsa;
+
+	public AlunoBolsista() {
+
+	}
+
+	public AlunoBolsista(Long matricula, String nome, double valorbolsa) {
+		super(matricula, nome);
+		this.valorbolsa = valorbolsa;
+	}
+
+	public double getValorbolsa() {
+		return valorbolsa;
+	}
+
+	public void setValorbolsa(double valorbolsa) {
+		this.valorbolsa = valorbolsa;
+	}
+
+}
