58f7f06436c77cc67f1b9e3ac8d6a7fe594ce0f8
Thiago Costa Santana <thikimlara@gmail.com>
2024-08-09 16:12:34 -0300

    commit app calculo

---
+++ app-calculo/src/module-info.java
@@ -0,0 +1,19 @@
+import br.com.cod3r.app.Calculadora;
+import br.com.cod3r.app.calculo.CalculadoraImpl;
+
+module app.calculo {
+	
+	requires app.logging;
+	exports br.com.cod3r.app.calculo;
+	
+//	exports br.com.cod3r.app.calculo.interno
+//	     to app.financeiro;
+	
+	opens br.com.cod3r.app.calculo to app.loogging, app.financeiro;
+	
+	requires app.api;
+	provides Calculadora
+	with CalculadoraImpl;
+	
+	
+}
